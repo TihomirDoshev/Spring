90c820901ed74d6cc69724b29ed31791bcfd2dd5
TihomirDoshev <122824906+TihomirDoshev@users.noreply.github.com>
2024-03-24 09:06:18 +0200

    1

--- 01.Spring Data - Java DB/7. SON Processing/src/main/java/com/example/exercise_7_jsonprocessing/constants/GlobalConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.exercise_7_jsonprocessing.constants;
-
-
-
-public class GlobalConstants {
-
-    public static final String RESOURCES_FILE_PATH = "src/main/resources/files/";
-}
--- 01.Spring Data - Java DB/8. XML Processing/src/main/java/com/example/xmlprocessing/util/ValidationUtil.java
+++
@@ -1,5 +0,0 @@
-package com.example.xmlprocessing.util;
-
-public interface ValidationUtil {
-    <T> boolean isValid(T entity);
-}
