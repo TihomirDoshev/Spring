fd3ad89d785e38b9642c3cc8846124ace3e4a1f0
TihomirDoshev <122824906+TihomirDoshev@users.noreply.github.com>
2024-05-29 11:56:15 +0300

    1

---
+++ Pathfinder/src/main/java/bg/softuni/pathfinder/web/HomeController.java
@@ -0,0 +1,21 @@
+package bg.softuni.pathfinder.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Random;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        double sofiaTemp = new Random().nextDouble();
+        double sozopolTemp = new Random().nextDouble();
+        model.addAttribute("sofiaTemperature",sofiaTemp);
+        model.addAttribute("sozopolTemp",sozopolTemp);
+        return "index";
+    }
+
+}
