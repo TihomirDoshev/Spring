4476e3b88cf204e2fa5c1696e8abdd10b063429b
TihomirDoshev <122824906+TihomirDoshev@users.noreply.github.com>
2024-06-04 10:25:48 +0300

    1

--- Projects/Mobilele/src/main/java/bg/softuni/mobilele/models/dto/UserRegistrationDTO.java
+++
@@ -1,7 +0,0 @@
-package bg.softuni.mobilele.models.dto;
-
-public record UserRegistrationDTO(String firstName,
-                                  String lastName,
-                                  String password,
-                                  String email) {
-}
--- Projects/Mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package bg.softuni.mobilele.service;
-
-import bg.softuni.mobilele.models.dto.UserRegistrationDTO;
-
-public interface UserService {
-    void registerUser(UserRegistrationDTO userRegistrationDTO);
-}
